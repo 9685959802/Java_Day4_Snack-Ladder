bec5ac85dedfd1f248c5c006ef7f0dd831364658
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-07 10:16:11 +0530

    Snack&Ladder Simulator

---
+++ SnackLader.java
@@ -0,0 +1,13 @@
+package snacklader;
+
+public class SnackLader {
+public static void main(String []args) {
+	final int position = 0;
+	
+	System.out.println("Snacklader simulator");
+	
+	byte player = 1;
+	
+	
+}
+}
