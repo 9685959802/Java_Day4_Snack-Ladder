7e0c0e8cfc49e4db6510407f94cc85ccb3c340ed
9685959802 <tusharbadgujar0000@gmail.com>
2022-09-17 08:32:08 +0530

    Repeat position upto 100

--- SnackLader.java
+++ SnackLader.java
@@ -1,6 +1,8 @@
 package snacklader;
 
 public class SnackLader {
+	static int positionUC1 = 1;
+	int positionUC2 = (int) ((Math.random() * 6) + 1);
 public static void main(String []args) {
 	final int position = 0;
 	
@@ -17,9 +19,64 @@ public static void main(String []args) {
 	//uc2
 		SnackLader dice = new SnackLader();
 		dice.diceRolled();
-	}
+		//UC3
+		
+		
+	SnackLader opt = new SnackLader();
+	    opt.optionCheck();
+
+}
+void OptionCheck() {
+	int  positionUC3 = (int) ((Math.random() * 6) + 1);
+	int checkOption = (int) (Math.random() * 3);
+	
+	
+switch(checkOption) {
+
+case 0 : System.out.println("No Play!...same position");
+		 break;
+case 1 : System.out.println("Yayyy...you climbed a ladder");
+		 positionUC1 = positionUC2 + positionUC3;
+         System.out.println("Position : On Square " + positionUC1 );
+		 break;
+case 2 : System.out.println("Oops...snake there");
+		 positionUC1 = positionUC2 - positionUC3;
+         if (positionUC1 <= 0)
+        	 positionUC1 = 1;
+         System.out.println("Position : On Square " + positionUC1);
+         break;
+}
+}
+
+
+
 	void diceRolled() {
 			int new_position = (int) ((Math.random() *6)+1);
-			System.out.println("Player moved to "+ new_position);
-}
-}
+			System.out.println("Player moved to "+ new_position);				
+	
+	}
+	void OptionCheck() {
+		int  positionUC3 = (int) ((Math.random() * 6) + 1);
+		int checkOption = (int) (Math.random() * 3);
+		      
+	switch(checkOption) {
+	
+	case 0 : System.out.println("No Play!...same position");
+			 break;
+	case 1 : System.out.println("Yayyy...you climbed a ladder");
+			 positionUC3 = positionUC3 + positionUC2;
+	         System.out.println("Position : On Square [" + positionUC3 + "]");
+			 break;
+	default : System.out.println("Oops...snake there");
+			 positionUC3 = positionUC2 - positionUC3;
+	         if (positionUC3 <= 0)
+	        	 positionUC3 = 1;
+	         System.out.println("Position : On Square [" + positionUC3 + "]");
+	         break;
+			}
+	}}
+		
+	
+
+
+	
